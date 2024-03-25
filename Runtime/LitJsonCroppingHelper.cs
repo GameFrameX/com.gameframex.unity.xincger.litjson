964d8c48ea35620b995636c3880f8a6e6a19048a
AlianBlank <wangfj11@foxmail.com>
2024-03-25 20:02:03 +0800

    [增加]1. 增加防裁剪帮助类

---
+++ Runtime/LitJsonCroppingHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace LitJson
+{
+    public class LitJsonCroppingHelper : UnityEngine.MonoBehaviour
+    {
+        public void Start()
+        {
+            _ = typeof(LitJson.ArrayMetadata);
+            _ = typeof(LitJson.Condition);
+            _ = typeof(LitJson.ExporterFunc);
+            _ = typeof(LitJson.ExporterFunc<>);
+            _ = typeof(LitJson.Extensions.Extensions);
+            _ = typeof(LitJson.Extensions.JsonIgnore);
+            _ = typeof(LitJson.FsmContext);
+            _ = typeof(LitJson.IJsonWrapper);
+            _ = typeof(LitJson.ImporterFunc);
+            _ = typeof(LitJson.ImporterFunc<,>);
+            _ = typeof(LitJson.JsonData);
+            _ = typeof(LitJson.JsonException);
+            _ = typeof(LitJson.JsonMapper);
+            _ = typeof(LitJson.JsonMockWrapper);
+            _ = typeof(LitJson.JsonReader);
+            _ = typeof(LitJson.JsonToken);
+            _ = typeof(LitJson.JsonType);
+            _ = typeof(LitJson.JsonWriter);
+            _ = typeof(LitJson.Lexer);
+            _ = typeof(LitJson.ObjectMetadata);
+            _ = typeof(LitJson.OrderedDictionaryEnumerator);
+            _ = typeof(LitJson.ParserToken);
+            _ = typeof(LitJson.PropertyMetadata);
+            _ = typeof(LitJson.UnityTypeBindings);
+            _ = typeof(LitJson.WrapperFactory);
+            _ = typeof(LitJson.WriterContext);
+        }
+    }
+}
